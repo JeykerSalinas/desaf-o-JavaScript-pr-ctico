44f87bbf73dbd822c2eb2876b48d970cae6f78d2
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-02-15 20:49:58 -0300

    Fixing bugs (thanks teacher)

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,17 +1,43 @@
 const nombre = document.getElementById("nombre");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
-const botton = document.getElementById("botton");
+const formulario = document.getElementById("formulario");
+const errorNombre = document.querySelector(".errorNombre");
+const errorAsunto = document.querySelector(".errorAsunto");
+const errorMensaje = document.querySelector(".errorMensaje");
+const resultado = document.querySelector(".resultado");
 
-botton.addEventListener("click",(event)=>{
-    console.log(document.getElementById("nombre").value)
-    event.preventDefault()
-})
+const regex = /[^a-z|\s]|^\s|^$/gi;
 
+const myFunction = (event) => {
+  event.preventDefault();
+  errorNombre.textContent = "";
+  errorAsunto.textContent = "";
+  errorMensaje.textContent = "";
+  resultado.textContent = "";
+  const nombreTrim = nombre.value.trim();
+  const asuntoTrim = asunto.value.trim();
+  const mensajeTrim = mensaje.value.trim();
 
+  if (regex.test(nombreTrim)) {
+    errorNombre.textContent = "El nombre es requerido";
+  }
+  if (regex.test(asuntoTrim)) {
+    errorAsunto.textContent = "El asunto es requerido";
+  }
+  if (regex.test(mensajeTrim)) {
+    errorMensaje.textContent = "El mensaje es requerido";
+  }
+  if (
+    !regex.test(nombreTrim) &&
+    !regex.test(asuntoTrim) &&
+    !regex.test(mensajeTrim)
+  ) {
+    resultado.textContent = "Mensaje enviado con éxito¡¡¡";
+  }
+  nombre.value = nombreTrim;
+  asunto.value = asuntoTrim;
+  mensaje.value = mensajeTrim;
+};
 
-
-// const cadena = "sds d";
-// const regex = /[^a-z|\s]/ig
-// result = regex.test(cadena)
-// console.log('result: ', result);
+formulario.addEventListener("submit", myFunction);
