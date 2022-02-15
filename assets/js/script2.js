ad73ded281d41898b868f99e255ee27c75f62bc0
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-02-15 16:49:57 -0300

    Erasing ejercicio2 button

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,17 +1,13 @@
 for (let i = 1; i <= document.querySelectorAll(".btn-change").length; i++) {
   const btn = document.getElementById(`btn-${i}`);
-  btn.addEventListener(
-    "click",
-    () =>
-      document.getElementById("caja").style.background =
-        btn.style.backgroundColor
-  );
+  btn.addEventListener("click", () => {
+    (
+      document.getElementById("caja").style.backgroundColor !=
+      btn.style.backgroundColor
+    ) ? 
+      document.getElementById("caja").style.backgroundColor =
+        btn.style.backgroundColor :
+      document.getElementById("caja").style.backgroundColor = "transparent";
+  });
 }
-
-const btnChange = document.getElementById("reset")
-btnChange.addEventListener("click", ()=>document.getElementById("caja").style.background =
-"transparent" )
-
-
-
 
